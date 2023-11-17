ddc9ec7b387cf24145b547a2c294691e5244a9ed
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-17 17:28:30 +0200

    add starter code

---
+++ app/src/main/java/com/example/sqlbasics/MainActivity.kt
@@ -0,0 +1,17 @@
+
+package com.example.sqlbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            AppDatabase.getDatabase(applicationContext).californiaParkDao().getAll()
+        }
+    }
+}
